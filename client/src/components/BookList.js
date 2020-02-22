1e3c4c1ff690a22e232e69a059762dc4a3b521d9
yjchoi_mac <yjchoi@yjchoi-macs-MacBook-Pro.local>
2020-02-23 00:01:34 +0900

    finishing frontend, fixing backend

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 const BookList = ({ data }) => {
+  const [selected, setSelected] = useState(null);
+
   const displayBooks = () => {
     if (data.loading) {
       return <div>Loading books</div>;
     } else {
-      return data.books.map(book => <li key={book.id}>{book.name}</li>);
+      return data.books.map(book => (
+        <li key={book.id} onClick={() => setSelected(book.id)}>
+          {book.name}
+        </li>
+      ));
     }
   };
 
   return (
     <div>
       <ul id='book-list'>{displayBooks()}</ul>
+      <BookDetails bookId={selected} />
     </div>
   );
 };
